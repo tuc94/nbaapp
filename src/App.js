53a221f0cb73d6fd0b32e99db7c17304470657ba
Baldecchi <tbaldecchi@C02XD70CJHD2.local>
2020-10-14 11:45:12 -0400

    Init commit

--- src/App.js
+++ src/App.js
@@ -5,19 +5,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <headers>Games Rosters Teams Players</headers>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         NBA Scoreboard
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
