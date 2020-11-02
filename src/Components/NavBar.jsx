55c237d0dd4a1f47b526e038c7dfeb479a1cfd37
Baldecchi <tbaldecchi@C02XD70CJHD2.local>
2020-11-02 15:09:37 -0500

    Began the task of creating the React Router

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,13 +1,63 @@
 import React from "react";
-import { BrowserRouter as Router, Router, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function NavBar(listofOptions) {
   let props = listofOptions;
   return (
     <Router>
-      <div></div>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Games</Link>
+            </li>
+            <li>
+              <Link to="/Rosters">Rosters</Link>
+            </li>
+            <li>
+              <Link to="/Teams">Teams</Link>
+            </li>
+            <li>
+              <Link to="/Players">Players</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/Rosters">
+            <Rosters />
+          </Route>
+          <Route path="/Teams">
+            <Teams />
+          </Route>
+          <Route path="/Players">
+            <Players />
+          </Route>
+          <Route path="/">
+            <Games />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
+}
+
+function Games() {
+  return <h2>Home</h2>;
+}
+
+function Rosters() {
+  return <h2>About</h2>;
+}
+
+function Teams() {
+  return <h2>Users</h2>;
+}
+
+function Players() {
+  return <h2>Players</h2>;
 }
 
 // <div>
