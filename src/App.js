eeb169c924df58a29ff4418f74b9b8d3ca4ed340
tuc94 <tuc.baldecchi@gmail.com>
2024-11-12 14:51:12 -0500

    cleaning up repo

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import NavBar from "./Components/NavBar";
-import CurrentDate from "./Components/CurrentDate";
-import TeamGameScoreCard from "./Components/TeamGameScoreCard/TeamGameScoreCard";
-import SelectDate from "./Components/SelectDate/SelectDate";
-import DatePicker from "react-datepicker";
-import axios from "axios";
-//Save this for later
-import HomePage from "./Containers/HomePage";
-import Standings from "./Containers/Standings";
-import SelectMe from "./Components/Select/SelectMe";
-import PlayerSearch from "./Components/PlayerSearch/PlayerSearch";
-import PlayerStatPage from "./Components/PlayerStatPage/PlayerStatPage";
+import React from 'react';
+import './App.css';
+import NavBar from './Components/NavBar';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div className="TitleTextBox">
-          <img src="nba.png"></img>
+          <img src="nba.png" />
           <div className="TitleText">
             <p>Tuc's NBA Scoreboard</p>
           </div>
         </div>
-        <NavBar listofOptions={["Games", "Rosters", "Teams", "Players"]} />
+        <NavBar listofOptions={['Games', 'Rosters', 'Teams', 'Players']} />
       </header>
     </div>
   );
