40763c7278db7c495796286ceb43123b695e5cf6
tuc94 <tuc.baldecchi@gmail.com>
2024-11-16 11:24:45 -0500

    began refactoring code for now

--- src/Components/PlayerStatPage/PlayerStatPage.jsx
+++ src/Components/PlayerStatPage/PlayerStatPage.jsx
@@ -1,52 +1,48 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Table } from "@material-ui/core";
-import Radio from "@material-ui/core/Radio";
+import { Table, TableHead, TableBody, TableRow, TableCell } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import SelectMe from "../Select/SelectMe";
-import Tooltip from "@material-ui/core/Tooltip";
 import "./PlayerStatPage.css";
 let seasonData = require("../../data/playerStatSeasonData.json");
 seasonData = seasonData.data.reverse();
 
-export default function PlayerStatPage(props) {
+export default function PlayerStatPage({ playerId, state, setState }) {
   const [playerStats, setPlayerStats] = useState([]);
   const [season, setSeason] = useState("2020");
   const [hidePlayersStats, setHidePlayersStats] = useState(false);
-  const playerID = props.playerId;
-  const state = props.state;
-  const setState = props.setState;
 
   const getPlayer = async () => {
-    //This doesn't work but want it as a frame of refrence
     const options = {
       method: "GET",
-      url: "season_averages",
-      params: { season: season, "player_ids[]": playerID },
+      url: "https://api.balldontlie.io/v1/season_averages",
+      params: { season, "player_ids[]": playerId },
       headers: {
         Accept: "application/json",
         "Access-Control-Allow-Origin": "*",
+        Authorization: "5453c273-add7-4732-b45a-e5163250cfaa"
       },
     };
 
-    axios
-      .request(options)
-      .then(function (response) {
-        if (response.data.data.length === 0) {
-          setHidePlayersStats(true);
-        } else if (response.data.data.length !== 0) {
-          setHidePlayersStats(false);
-          setPlayerStats(response.data.data);
-        }
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+    try {
+      const response = await axios.request(options);
+      const data = response.data?.data || [];
+
+      if (data.length === 0) {
+        setHidePlayersStats(true);
+      } else {
+        setHidePlayersStats(false);
+        setPlayerStats(data);
+      }
+    } catch (error) {
+      console.error("Error fetching player data:", error);
+      setHidePlayersStats(true);
+    }
   };
 
   useEffect(() => {
     getPlayer();
-  }, [playerID, season]);
+  }, [playerId, season]);
 
   return (
     <div>
@@ -54,60 +50,60 @@ export default function PlayerStatPage(props) {
         <Button
           variant="contained"
           color="primary"
-          onClick={(e) => setState(false)}
+          onClick={() => setState(false)}
           className="backButtonSize"
         >
-          <div className="backButtonStyle">Back </div>
+          <div className="backButtonStyle">Back</div>
         </Button>
       </div>
-      <div></div>
       <SelectMe
         data={seasonData}
-        label={"Season"}
+        label="Season"
         state={season}
         setState={setSeason}
       />
-      {season}
       {hidePlayersStats ? (
-        <div className="errorMessage"> Search another player </div>
+        <div className="errorMessage">Search another player</div>
       ) : (
         <Table>
-          <tr>
-            <th>AST</th>
-            <th>BLK</th>
-            <th>DRB</th>
-            <th>3P%</th>
-            <th>3PA</th>
-            <th>3P</th>
-            <th>FT%</th>
-            <th>G</th>
-            <th>MP</th>
-            <th>ORB</th>
-            <th>PTS</th>
-            <th>TRB</th>
-            <th>STL</th>
-            <th>TOV</th>
-          </tr>
-          {playerStats.map((player) => {
-            return (
-              <tr>
-                <td>{player.ast}</td>
-                <td>{player.blk} </td>
-                <td>{player.dreb} </td>
-                <td>{player.fg3_pct} </td>
-                <td>{player.fg3a}</td>
-                <td>{player.fg3m} </td>
-                <td>{player.ft_pct} </td>
-                <td>{player.games_played} </td>
-                <td>{player.min}</td>
-                <td>{player.oreb} </td>
-                <td>{player.pts} </td>
-                <td>{player.reb} </td>
-                <td>{player.stl}</td>
-                <td>{player.turnover} </td>
-              </tr>
-            );
-          })}
+          <TableHead>
+            <TableRow>
+              <TableCell>AST</TableCell>
+              <TableCell>BLK</TableCell>
+              <TableCell>DRB</TableCell>
+              <TableCell>3P%</TableCell>
+              <TableCell>3PA</TableCell>
+              <TableCell>3P</TableCell>
+              <TableCell>FT%</TableCell>
+              <TableCell>G</TableCell>
+              <TableCell>MP</TableCell>
+              <TableCell>ORB</TableCell>
+              <TableCell>PTS</TableCell>
+              <TableCell>TRB</TableCell>
+              <TableCell>STL</TableCell>
+              <TableCell>TOV</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {playerStats.map((player) => (
+              <TableRow key={player.player_id || Math.random()}>
+                <TableCell>{player.ast}</TableCell>
+                <TableCell>{player.blk}</TableCell>
+                <TableCell>{player.dreb}</TableCell>
+                <TableCell>{player.fg3_pct}</TableCell>
+                <TableCell>{player.fg3a}</TableCell>
+                <TableCell>{player.fg3m}</TableCell>
+                <TableCell>{player.ft_pct}</TableCell>
+                <TableCell>{player.games_played}</TableCell>
+                <TableCell>{player.min}</TableCell>
+                <TableCell>{player.oreb}</TableCell>
+                <TableCell>{player.pts}</TableCell>
+                <TableCell>{player.reb}</TableCell>
+                <TableCell>{player.stl}</TableCell>
+                <TableCell>{player.turnover}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
         </Table>
       )}
     </div>
