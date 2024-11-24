8b5c7fe0002b8182e7ddedd34f3ac41e26dc6d6c
tuc94 <tuc.baldecchi@gmail.com>
2024-11-24 10:32:30 -0500

    added test

--- src/Components/StandingsTable/StandingsTable.jsx
+++ src/Components/StandingsTable/StandingsTable.jsx
@@ -1,35 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Typography,
+} from "@mui/material";
 
 export default function StandingsTable(props) {
   const standingData = props.standings;
-  let confrenceName = "";
+  let conferenceName = "";
+
   if (standingData.length !== 0) {
-    confrenceName = standingData[0].group.name;
+    conferenceName = standingData[0].group.name;
   }
-  console.log(standingData[0]);
+
   return (
     <div>
-      <p>{confrenceName}</p>
-      <React.Fragment>
-        <tr>
-          <th>Seed</th>
-          <th>Team</th>
-          <th>Wins</th>
-          <th>Loses</th>
-          <th>Win Percentages</th>
-        </tr>
-        {standingData.map((teamStanding) => {
-          return (
-            <tr>
-              <td>{teamStanding.position}</td>
-              <td>{teamStanding.team.name}</td>
-              <td>{teamStanding.games.win.total}</td>
-              <td>{teamStanding.games.lose.total}</td>
-              <td>{teamStanding.games.win.percentage}</td>
-            </tr>
-          );
-        })}
-      </React.Fragment>
+      <Typography variant="h6" gutterBottom>
+        {conferenceName}
+      </Typography>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Seed</TableCell>
+              <TableCell>Team</TableCell>
+              <TableCell>Wins</TableCell>
+              <TableCell>Loses</TableCell>
+              <TableCell>Win Percentage</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {standingData.map((teamStanding) => (
+              <TableRow key={teamStanding.team.name}>
+                <TableCell>{teamStanding.position}</TableCell>
+                <TableCell>{teamStanding.team.name}</TableCell>
+                <TableCell>{teamStanding.games.win.total}</TableCell>
+                <TableCell>{teamStanding.games.lose.total}</TableCell>
+                <TableCell>{teamStanding.games.win.percentage}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
